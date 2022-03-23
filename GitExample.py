7df90cb302648e523ca8b9e9fed99b5cf7003506
Alejandro Quesada C <49792796+AleQuesada2012@users.noreply.github.com>
2022-03-23 16:57:58 -0600

    Añade el primer archivo del repositorio
    Este es el archivo que se usará como ejemplo para describir funciones de control de versiones de git con GitHub.

---
+++ GitExample.py
@@ -0,0 +1,51 @@
+print("Hello world!")
+
+#diferentes funciones
+def suma (valor1, valor2):
+    try:
+        result  = valor1 + valor2
+        print("Resultado de la suma: " + str(result))
+        return result
+    except:
+        print("los valores deben ser tipos numéricos que se puedan sumar")
+    return 0
+
+def resta (valor1, valor2):
+    try:
+        result = valor1 - valor2
+        print("Resultado de la resta: " + str(result))
+        return result
+    except:
+        print("Los valores deben ser tipos numéricos que se puedan restar")
+    return 0
+
+def multi ( valor1, valor2):
+    try:
+        result = valor1 * valor2
+        print("Resultado de la multiplicación: " + str(result))
+        return result
+    except:
+        print ("Los valores deben ser tipos numéricos que se puedan multiplicar")
+    return 0
+
+def producto_punto(vector1, vector2):
+    try:
+        # asumiendo que los dos vectores sean del mismo largo
+        result = 0
+        for i in range(len(vector1)):
+            result += (vector1[i] * vector2[i])
+        print(result)
+        return  result
+    except:
+        print("Los valores de entrada deben ser listas de una dimensión y del mismo largo")
+    return 0
+
+# Valores de prueba
+TestVec1 = [1, 5, 8, 10]
+TestVec2 = [-1, 50, 20, 2]
+
+#invocaciones de prueba
+suma(10, -50)
+resta(-5, -5)
+multi(7, 3)
+producto_punto(TestVec1, TestVec2)
